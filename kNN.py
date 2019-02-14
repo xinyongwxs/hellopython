b904ab0c78e71c48a2928b9420851b74a0166509
Wang Stonna <stonna.wang@sap.com>
2019-02-14 16:59:06 +0800

    Add kNN dataset testing function

--- kNN.py
+++ kNN.py
@@ -9,6 +9,11 @@ def createDataSet():
 
 
 def classify0(inX, dataSet, labels, k):
+    # inX: The input vector to classify;
+    # dataSet: Our full matrix of training examples;
+    # labels: A vector of labels;The labels vector
+    # should have as many elements in it as there are rows in the dataSet matrix.
+    # k: The number of nearest neighbors to use in the voting.
     dataSetSize = dataSet.shape[0]
     diffMat = tile(inX, (dataSetSize, 1)) - dataSet
     sqDiffMat = diffMat ** 2
@@ -20,7 +25,54 @@ def classify0(inX, dataSet, labels, k):
         voteILabel = labels[sortedDistIndicies[i]]
         classCount[voteILabel] = classCount.get(voteILabel, 0) + 1
     sortedClassCount = sorted(
-        classCount.iteritems(), key=operator.itemgetter(1), reverse=True
+        classCount.items(), key=operator.itemgetter(1), reverse=True
     )
     return sortedClassCount[0][0]
 
+
+def file2matrix(filename):
+    fr = open(filename)
+    numberOfLines = len(fr.readlines())
+    returnMat = zeros((numberOfLines, 3))
+    classLabelVector = []
+    fr = open(filename)
+    index = 0
+    for line in fr.readlines():
+        line = line.strip()
+        listFromLine = line.split("\t")
+        returnMat[index, :] = listFromLine[0:3]
+        classLabelVector.append(int(listFromLine[-1]))
+        index += 1
+    return returnMat, classLabelVector
+
+
+def autoNorm(dataSet):
+    minVals = dataSet.min(0)
+    maxVals = dataSet.max(0)
+    ranges = maxVals - minVals
+    normDataSet = zeros(shape(dataSet))
+    m = dataSet.shape[0]
+    normDataSet = dataSet - tile(minVals, (m, 1))
+    normDataSet = normDataSet / tile(ranges, (m, 1))
+    return normDataSet, ranges, minVals
+
+
+def datingClassTest():
+    hoRatio = 0.10
+    datingDataMat, datingLabels = file2matrix("datingTestSet2.txt")
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    m = normMat.shape[0]
+    numTestVecs = int(m * hoRatio)
+    errorCount = 0.0
+    for i in range(numTestVecs):
+        classifierResult = classify0(
+            normMat[i, :], normMat[numTestVecs:m, :], datingLabels[numTestVecs:m], 3
+        )
+        print(
+            "the classifier came back with: %d, the real answer is: %d"
+            % (classifierResult, datingLabels[i])
+        )
+        if classifierResult != datingLabels[i]:
+            errorCount += 1.0
+    print("the total error rate is: %f" % (errorCount / float(numTestVecs)))
+    print(errorCount)
--- main.py
+++ main.py
@@ -1,6 +1,31 @@
 import kNN
+from numpy import *
+import matplotlib.pyplot as plt
 
-group, labels = kNN.createDataSet()
+# import matplotlib
 
-print(group)
-print(labels)
+# group, labels = kNN.createDataSet()
+
+# print(kNN.classify0([0, 0], group, labels, 3))
+
+# test = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+# test0 = zeros((4, 3))
+# test1 = array([array([1, 2, 3]), array([4, 5, 6]), array([7, 8, 9])])
+# print(test0[2, :])
+# print(test[2][:])
+# print(test1[2, :])
+
+# datingDataMat, datingLabels = kNN.file2matrix("datingTestSet2.txt")
+# fig = plt.figure()
+# ax = fig.add_subplot(111)
+# ax.scatter(
+#     datingDataMat[:, 0],
+#     datingDataMat[:, 1],
+#     array(datingLabels),
+#     10.0 * array(datingLabels),
+# )
+# plt.show()
+# print(datingDataMat)
+# print(datingLabels)
+
+kNN.datingClassTest()
